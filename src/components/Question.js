0987b1bc57bd1f06fbc96321395158c0b73861c8
wminbandara <wminbandara@gmail.com>
2025-02-04 21:57:19 +0530

    Displaying Questions

---
+++ src/components/Question.js
@@ -0,0 +1,21 @@
+import Options from "./Options";
+
+function Question({ question }) {
+  //   console.log(question);
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} />
+
+      {/* <div className="options">
+        {question.options.map((option) => (
+          <button className="btn btn-option" key={option}>
+            {option}
+          </button>
+        ))}
+      </div> */}
+    </div>
+  );
+}
+
+export default Question;
